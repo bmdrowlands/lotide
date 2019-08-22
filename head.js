3ff84194dda56f4009d195c1fbd6e831fad0ca54
Benjamin Deverell-Rowlands <b.m.d.rowlands@gmail.com>
2019-08-22 14:39:34 +0000

    head

--- head.js
+++ head.js
@@ -8,9 +8,10 @@ const assertEqual = function(actual, expected) {
 
 };
 
-function functionName(parameters) {
-  code to be executed
-}
+function head(array){
+     return array[0]
+
+   }
 // TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

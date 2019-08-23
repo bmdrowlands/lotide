6df501c124d600e66c6e1b9c42320c5b21ec3924
Benjamin Deverell-Rowlands <b.m.d.rowlands@gmail.com>
2019-08-23 17:11:26 +0000

    eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,22 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-   if (actual === expected){
-     console.log('😍😍😍', 'Assertion Passed:', `[${actual}] === [${expected}]`)
-   } else {
-     console.log('🤢🤢🤢', 'Assertion Failed:', `[${actual}] !== [${expected}]`)
-   }
-
-};
-
-const eqArrays = function(array1, array2){
-
-  for ( i = 0; i < array1.length; i++){
-    for (a = 0; a < array2.length; a++){
-      if (array1[i] === array2[a]) {
-        return true
-      }
-    }
+  if (actual === expected) {
+    console.log('😍😍😍', 'Assertion Passed:', `[${actual}] === [${expected}]`);
+  } else {
+    console.log('🤢🤢🤢', 'Assertion Failed:', `[${actual}] !== [${expected}]`);
   }
+
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+//test
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
